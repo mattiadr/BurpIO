0db85bc8051001c9beb0cf8a2a1ac7f14c710f15
Mattia De Rosa <mattiadr96@gmail.com>
2026-09-08 15:00:34 +0200

    removed auto-repeater feature

--- src/main/kotlin/com/mattiadr/burpIO/BurpIO.kt
+++ src/main/kotlin/com/mattiadr/burpIO/BurpIO.kt
@@ -3,12 +3,8 @@ package com.mattiadr.burpIO
 import burp.api.montoya.BurpExtension
 import burp.api.montoya.MontoyaApi
 import burp.api.montoya.ui.swing.SwingUtils
-import com.mattiadr.burpIO.modules.BurpIOHttpHandler
 import com.mattiadr.burpIO.modules.BurpIOMenuItemProvider
 import com.mattiadr.burpIO.modules.BurpIOQuickSession
-import javax.swing.JCheckBoxMenuItem
-import javax.swing.JMenu
-import javax.swing.JMenuItem
 
 @Suppress("unused")
 class BurpIO : BurpExtension {
@@ -22,25 +18,8 @@ class BurpIO : BurpExtension {
 		val menuItemProvider = BurpIOMenuItemProvider()
 		api.userInterface().registerContextMenuItemsProvider(menuItemProvider)
 
-		// setup http handler
-		val httpHandler = BurpIOHttpHandler()
-		api.http().registerHttpHandler(httpHandler)
-
 		// setup hotkeys
 		BurpIOQuickSession.setupHotKeys()
-
-		// setup suite menu
-		val topMenu = JMenu("BurpIO")
-		JCheckBoxMenuItem("Auto Repeater").apply {
-			isSelected = false
-			addActionListener { AppContext.autoRepeater = this.isSelected }
-			topMenu.add(this)
-		}
-		JMenuItem("Copy Auto Repeater Bruno Script").apply {
-			addActionListener { stringToClipboard("req.setHeader(\"X-Burp-Repeater\", req.getName())") }
-			topMenu.add(this)
-		}
-		api.userInterface().menuBar().registerMenu(topMenu)
 
 		// register settings
 		api.userInterface().registerSettingsPanel(Settings.buildSettingsPanel())
@@ -51,8 +30,6 @@ class BurpIO : BurpExtension {
 object AppContext {
 	lateinit var api: MontoyaApi private set
 	lateinit var swingUtils: SwingUtils private set
-
-	var autoRepeater: Boolean = false
 
 	fun init(api: MontoyaApi) {
 		this.api = api
